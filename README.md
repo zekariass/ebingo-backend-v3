35fdf929df5675140e7a12ed1fccf38dd5746d3d
Zekarias Semegnew Negese <zemaedot3@gmail.com>
2026-04-02 14:31:01 +0100

    Some updtae

--- README.md
+++
@@ -1,6 +0,0 @@
-# Ebingo Game Backend
-
-This is the backend for the Ebingo game, built using Spring webflux. It provides APIs for user management, game
-management, and real-time game updates.
-
-The game is under development.
--- src/main/java/com/ebingo/backend/game/service/autoplay/AutoPlayAdminController.java
+++ src/main/java/com/ebingo/backend/game/service/autoplay/AutoPlayAdminController.java
@@ -1,11 +1,15 @@
 package com.ebingo.backend.game.service.autoplay;
 
+import com.ebingo.backend.common.annotation.RequireAccessToken;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin/autoplay")
 @RequiredArgsConstructor
+@RequireAccessToken
+@Slf4j
 public class AutoPlayAdminController {
 
     private final GlobalAutoPlayManager manager;
